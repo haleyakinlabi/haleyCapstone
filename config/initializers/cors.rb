4e9e20c32ff95d5bf63be2c6b4e62bd96ba1c54d
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-01-16 11:34:15 -0700

    Expose the token header to clients

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource "*",
+      headers: :any, # allowed request headers
+      expose: ["access-token"], # response headers exposed to clients
+      methods: [:get, :post, :put, :patch, :delete, :options, :head] # allowed HTTP verbs for requests
   end
 end
