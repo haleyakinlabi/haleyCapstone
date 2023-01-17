4403a7c795d528ee2f063fb9cd62aa91c96e05de
Nate Hopkins (hopsoft) <natehop@gmail.com>
2023-01-17 13:03:01 -0700

    Return user on register and authentication

--- app/controllers/users/authentication_controller.rb
+++ app/controllers/users/authentication_controller.rb
@@ -6,14 +6,16 @@ module Users
     # before_action :authenticate_resource, only: [:destroy]
 
     def create
-      super
-      resource.update_columns token_issued_at: Time.current
-      #   if resource.authenticate(params[:password])
-      #     create_token_and_set_header(resource, resource_name)
-      #     render_success(message: I18n.t('api_guard.authentication.signed_in'))
-      #   else
-      #     render_error(422, message: I18n.t('api_guard.authentication.invalid_login_credentials'))
-      #   end
+      respond_to do |format|
+        if resource.authenticate(params[:password])
+          create_token_and_set_header(resource, resource_name)
+          resource.update_columns token_issued_at: Time.current
+          @user = resource.reload
+          format.json { render "users/show", status: :created, location: @user }
+        else
+          format.json { render json: {message: I18n.t("api_guard.authentication.invalid_login_credentials")}, status: :unprocessable_entity }
+        end
+      end
     end
 
     def destroy
--- app/controllers/users/registration_controller.rb
+++ app/controllers/users/registration_controller.rb
@@ -4,15 +4,27 @@ module Users
   class RegistrationController < ApiGuard::RegistrationController
     # before_action :authenticate_resource, only: [:destroy]
 
-    # def create
-    #   init_resource(sign_up_params)
-    #   if resource.save
-    #     create_token_and_set_header(resource, resource_name)
-    #     render_success(message: I18n.t('api_guard.registration.signed_up'))
-    #   else
-    #     render_error(422, object: resource)
-    #   end
-    # end
+    def create
+      init_resource(sign_up_params)
+
+      respond_to do |format|
+        if resource.save
+          create_token_and_set_header(resource, resource_name)
+          resource.update_columns token_issued_at: Time.current
+          @user = resource.reload
+          format.json { render "users/show", status: :created, location: @user }
+        else
+          format.json { render json: resource.errors, status: :unprocessable_entity }
+        end
+      end
+
+      #   if resource.save
+      #     create_token_and_set_header(resource, resource_name)
+      #     render_success(message: I18n.t('api_guard.registration.signed_up'))
+      #   else
+      #     render_error(422, object: resource)
+      #   end
+    end
 
     # def destroy
     #   current_resource.destroy
